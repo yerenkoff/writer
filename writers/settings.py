2f6e95b7f19477344963fca4d20cee2c4556a4b9
Kirill Yerenkoff <yerenkoff@gmail.com>
2020-02-02 15:43:28 +0300

    First commit

--- writers/settings.py
+++ writers/settings.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "wapp/media")
 
 import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
