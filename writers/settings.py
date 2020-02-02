ba90bc891c454a27b4709a577670d7f0867a503d
Kirill Yerenkoff <yerenkoff@gmail.com>
2020-02-02 15:45:34 +0300

    First commit

--- writers/settings.py
+++ writers/settings.py
@@ -134,5 +134,5 @@ import dj_database_url
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'wapp/static'),
 )
